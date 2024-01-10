3ff32db7b618a84d2515849b6bd76083857622c8
isa <isaahmedshan190138@gmail.com>
2024-01-10 21:58:54 +0600

    jwt and private route remind

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,14 +8,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAkmtSTCZ7C8lTDcQrQAnEIdEpV5bXgfOo",
-//   authDomain: "group-study-assignment-a7832.firebaseapp.com",
-//   projectId: "group-study-assignment-a7832",
-//   storageBucket: "group-study-assignment-a7832.appspot.com",
-//   messagingSenderId: "402379630983",
-//   appId: "1:402379630983:web:45113a0e4496e756c20b223309764888",
-// };
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -24,14 +16,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY
-//   authDomain: 
-//   projectId: 
-//   storageBucket:
-//   messagingSenderId: 
-//   appId:
-// };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
